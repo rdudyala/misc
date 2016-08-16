020300a5d3bcd0cdd68b8ca9718bdaf3dc09523c
rdudyala <rdudyala@gmail.com>
2016-08-16 10:42:56 +0000

    added new file with sleep for init scirpt

---
+++ zxceilostartup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo sed -i "s/.*127.0.0.1.*/127.0.0.1 localhost $(hostname)/" /etc/hosts
+/home/ubuntu/openstack-scripts/ceilometer_init_script.sh
+echo $PWD
+cd /home/ubuntu/pub-sub/ceilometer_pub_sub
+sleep 5
+nohup ./sub_main.py &
+echo $PWD
+cd -
+echo $PWD
